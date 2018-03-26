7d3ad615801a9b71cf7d6f84e4e9f86a62e02625
gepingli <1435813129@qq.com>
2018-03-26 20:02:19 +0800

    加入拖拽后的响应   音频时间和定时器出现bug待解决

--- assets/js/moveBar.js
+++ assets/js/moveBar.js
@@ -9,22 +9,26 @@ var musicPlay = (function() {
         oNum.state = 0;
         oNum.timeNum = null;
         oNum.oTime = 0;
+        oNum.perTime = 0;
     };
-    document.getElementsByClassName('line2')[0].style.width = '0px';
     //设置页面
     var setHTML = function(str) {
+        clearInterval(oNum.timeNum);
+        document.getElementsByClassName('line2')[0].style.width = '0px';
+        document.getElementsByClassName('songTimeEnd')[0].innerHTML = '0:00';
         oNum.oMusic = new Audio(oNum.oObj[str].musicPosition);
-        oNum.oMusic.play();
-        console.log(oNum.oMusic.currentTime);
         oNum.state = 1;
         oNum.oMusic.oncanplay = function() {
+            oNum.oMusic.play();
             oNum.oTime = parseInt(oNum.oMusic.duration);
             setBeforeTime('songTimeEnd', 'end');
+            oNum.perTime = parseFloat(document.getElementsByClassName('wai')[0].clientWidth) / (10 * oNum.oTime);
             oNum.oDiv.autoPlay(oNum.oTime);
         }
         document.getElementsByClassName('songName')[0].innerHTML = oNum.oObj[str].songName;
         document.getElementsByClassName('singerName')[0].innerHTML = oNum.oObj[str].singer;
-        document.getElementsByClassName('songTimeEnd')[0].innerHTML = oNum.oMusic.duration;
+        document.getElementById('circle').style.background = 'url(' + oNum.oObj[str].bk + ')';
+        document.getElementById('circle').style.backgroundSize = '100% 100%';
     };
 
 
@@ -60,7 +64,7 @@ var musicPlay = (function() {
     var drag = function(obj, callback) {
         obj.parentNode.ontouchstart = function(ev) {
                 ev = ev || event;
-                obj.style.left = ev.touches[0].clientX - this.offsetLeft + 'px';
+                obj.style.left = ev.touches[0].clientX - this.offsetLeft - 7.5 + 'px';
                 change(obj, callback);
 
                 var x = ev.touches[0].clientX - obj.offsetLeft;
@@ -92,29 +96,42 @@ var musicPlay = (function() {
                         change(obj, callback);
                     }
                     //释放
-                document.ontouchend = function() {
+                document.getElementsByClassName('wai')[0].ontouchend = function() {
                     this.onmousemove = null;
                     this.onmouseup = null;
                     console.log('stop');
+                    var oLi2 = document.getElementsByClassName('line2')[0];
+                    oLi2.style.width = obj.style.left;
+                    var percent = obj.offsetLeft / (obj.parentNode.clientWidth - obj.offsetWidth);
+                    //console.log(percent);
+                    oNum.oMusic.currentTime = oNum.oTime * percent;
+                    //console.log(oNum.oMusic.currentTime);
+                    clearInterval(oNum.timeNum);
+                    oNum.timeNum = null;
+                    oNum.oDiv.autoPlay(oNum.oTime);
+                    console.log(oNum.timeNum);
                 }
                 return false;
             }
             //自动行进
 
         obj.autoPlay = function(time) {
+            //0
             var oLi2 = document.getElementsByClassName('line2')[0];
+            //550
             var oW = document.getElementsByClassName('wai')[0];
-            var perTime = parseFloat(oW.clientWidth) / time;
-            console.log(perTime);
             oNum.timeNum = setInterval(function() {
-                oLi2.style.width = parseFloat(oLi2.style.width) + perTime / 10 + 'px';
-                oNum.oDiv.style.left = oLi2.style.width;
+                oLi2.style.width = parseFloat(oLi2.style.width) + oNum.perTime + 'px';
+                console.log(oLi2.style.width + '' + oNum.perTime);
+                console.log(oNum.timeNum);
+                oNum.oDiv.style.left = parseFloat(oLi2.style.width) + 'px';
+                console.log(oNum.oDiv.style.left);
                 setBeforeTime('songTimeStarted', 'start');
-                if (parseFloat(oNum.oDiv.style.left) + 7.5 >= parseFloat(oW.clientWidth)) {
+                if (parseFloat(oNum.oDiv.style.left) >= parseFloat(oW.clientWidth)) {
                     oNum.oMusic.currentTime = 0;
-                    oNum.oDiv.style.left = 0 + 'px';
+                    oNum.oDiv.style.left = -7.5 + 'px';
                     oLi2.style.width = 0 + 'px';
-
+                    oNum.oMusic.play();
                 }
             }, 100);
             callback || callback();
@@ -134,7 +151,7 @@ var musicPlay = (function() {
                 oNum.oObj = data;
                 setHTML(str);
                 drag(oNum.oDiv, function(v) {
-                    console.log(v);
+                    //  console.log(v);
                 });
             }
         });
